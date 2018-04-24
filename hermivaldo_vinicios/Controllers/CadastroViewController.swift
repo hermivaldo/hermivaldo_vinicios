2c4c2c602e8a73ea309890ea0472f1bf8fe53c61
Usuário Convidado <Guest@pa06micro39.local>
2018-04-23 22:42:20 -0300

    Finalização das telas

---
+++ hermivaldo_vinicios/Controllers/CadastroViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CadastroViewController.swift
+//  hermivaldo_vinicios
+//
+//  Created by Usuário Convidado on 23/04/18.
+//  Copyright © 2018 fiap. All rights reserved.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var tvProdName: UITextField!
+    @IBOutlet weak var tvState: UITextField!
+    @IBOutlet weak var tvPrice: UITextField!
+    @IBOutlet weak var sCard: UISwitch!
+    
+    
+    // MARK: - methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - IOBouds
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
